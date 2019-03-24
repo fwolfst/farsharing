e584f0dba8d43b189166ac36d1cee30f5f93a8cf
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2019-03-24 16:31:49 +0100

    add some dummy seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,39 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# ddd - dummy demo data
+
+# Users
+admin_user = User.new(email: 'admin@farsharing.domain',
+                      password: 'adminadmin',
+                      password_confirmation: 'adminadmin',
+                      confirmed_at: DateTime.now - 1)
+
+user_user = User.create(email: 'user@farsharing.domain',
+                        password: 'useruser',
+                        password_confirmation: 'useruser',
+                        confirmed_at: DateTime.now - 1)
+
+[admin_user, user_user].each do |user|
+  user.skip_confirmation!
+  user.save!
+end
+
+
+# Features
+pharmacy_feature = Feature.new(name: 'Pharmacy')
+supermarket_feature = Feature.new(name: 'Supermarket')
+
+[pharmacy_feature, supermarket_feature].each do |feature|
+  feature.save!
+end
+
+
+# Locations
+small_ville = Location.new(name: 'Smallville', features: [pharmacy_feature])
+big_ville = Location.new(name: 'Metropolis', features: [pharmacy_feature, supermarket_feature])
+
+[small_ville, big_ville].each do |location|
+  location.save!
+end
+
+# Cars
+small_car = Car.new(name: 'Beetly', manufacture: 'VW', make: 'Beetle', license_plate: 'SAW - SL 1', num_seats: 3, description: 'The coolest car unalive')
+big_car = Car.new(name: 'Roove', license_plate: 'SAW - SL 8', num_seats: 5)
