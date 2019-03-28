ef2ec1c5ba8cd02726a926d30cbd3aba03fc351c
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2019-03-28 22:41:03 +0100

    set default times for events

--- app/models/event.rb
+++ app/models/event.rb
@@ -18,4 +18,11 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User', default: -> { Current.user }
 
   scope :between, ->(time_range) { where("start_time <= ? AND end_time >= ?", time_range.end, time_range.begin) }
+
+  after_initialize :set_default_times
+
+  def set_default_times
+    self.start_time ||= DateTime.now
+    self.end_time   ||= self.start_time + 1
+  end
 end
