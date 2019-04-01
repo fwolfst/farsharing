7a2a9b8063b4c61f77fffce52d56be76922452f4
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2019-04-01 20:53:53 +0200

    PageController#*: collapse to one-line methods

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,18 +1,13 @@
 class PagesController < ApplicationController
   skip_verify_authorized
 
-  def index
-  end
+  def index ; end
 
-  def about
-  end
+  def about ; end
 
-  def impressum
-  end
+  def impressum ; end
 
-  def contact
-  end
+  def contact ; end
 
-  def privacy
-  end
+  def privacy ; end
 end
